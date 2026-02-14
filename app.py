28f078e151d90dec0dce722529d1c1ccb6344b62
sahiltiwari03041990 <2025AA05769@wilp.bits-pilani.ac.in>
2026-02-14 14:33:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Heart Disease Prediction", layout="wide")
+
+st.title("🫀 Heart Disease Prediction – ML Models")
+
+st.sidebar.header("Configuration")
+model_name = st.sidebar.selectbox(
+    "Select Model",
+    ["logistic_regression","decision_tree","knn","naive_bayes","random_forest","xgboost"]
+)
+
+file_path = "data/heart_test_with_target.csv"
+
+uploaded_file = st.file_uploader("Upload CSV test data", type=["csv"])
+
+with open(file_path, "rb") as f:
+        csv_data = f.read()
+
+st.download_button(
+        label="⬇️ Download Test CSV",
+        data=csv_data,
+        file_name="test_dataset_without_target.csv",
+        mime="text/csv"
+    )
+
+
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    if 'target' in data.columns:
+        X = data.drop('target', axis=1)
+        y = data['target']
+    else:
+        X = data
+        y = None
+
+    model = joblib.load(f"model/{model_name}.pkl")
+    preds = model.predict(X)
+
+
+    if y is not None:
+        st.subheader("Evaluation Metrics")
+        report = classification_report(y, preds, output_dict=True)
+        st.dataframe(pd.DataFrame(report).T)
+
+        cm = confusion_matrix(y, preds)
+        fig, ax = plt.subplots()
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=ax)
+        ax.set_xlabel("Predicted")
+        ax.set_ylabel("Actual")
+        st.pyplot(fig)
---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+# define features with numerical and categorical values
+numerical_cols = [ 'age', 'trestbps' ,'chol', 'thalach']
+
+categorical_cols = [ 'sex', 'cp', 'fbs', 'restecg','exang','oldpeak','slope','ca','thal']
+
+# define preprocessor to scale and encode the features and split the dataset
+def get_preprocessor():
+    return ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numerical_cols),
+            ("cat", OneHotEncoder(handle_unknown="ignore", sparse_output=False),
+             categorical_cols)
+        ]
+    )
